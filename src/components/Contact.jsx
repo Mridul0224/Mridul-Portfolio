44f13ec66a3a029ccfdebf3a2b0306342ee1dbcc
Mridul Ajaykumar <mridul@bu.edu>
2024-03-03 22:36:27 -0500

    changes to about section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,16 +29,16 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'service_hnmm7tr', // paste your ServiceID here (you'll get one when your service is created).
+        'template_njhf4qc', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Mridul Ajaykumar', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'mridulmalayilvalupil07@gmail.com', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        '3uFlZa7wJJtSZ1paW' //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
